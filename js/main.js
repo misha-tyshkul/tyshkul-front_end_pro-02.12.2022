2787750fd789e1ce96cd7e57bc49858966932e05
Misha Tyshkul <tyshkulm@gmail.com>
2022-12-03 13:00:11 +0200

    Replacing Conditions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 const value = prompt(
   "Какую арифметическую операцию вы хотите выполнить:\n 1. add - добавление;\n 2. sub - вычитание;\n 3. mult - умножение;\n 4. div - деление."
 );
+
 const num1 = parseInt(prompt("Введите первое число"));
 const num2 = parseInt(prompt("Введите второе число"));
 
 if (value === "add") {
-  alert(`${num1} + ${num2} = ${num1 + num2}`);
+  alert(`${num1} - ${num2} = ${num1 + num2}`);
 } else if (value === "sub") {
-  alert(`${num1} - ${num2} = ${num1 - num2}`);
+  alert(`${num1} + ${num2} = ${num1 - num2}`);
 } else if (value === "mult") {
-  alert(`${num1} * ${num2} = ${num1 * num2}`);
+  alert(`${num1} / ${num2} = ${num1 * num2}`);
 } else if (value === "div") {
-  alert(`${num1} / ${num2} = ${num1 / num2}`);
+  alert(`${num1} * ${num2} = ${num1 / num2}`);
 } else {
   alert("Запишите запрос операции внимательнее");
 }
